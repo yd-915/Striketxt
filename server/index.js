9637b5d77113e6370489286ef1591869828ba663
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-06 21:32:32 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const passport = require('passport')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const db = require('./db')
 const sessionStore = new SequelizeStore({db})
-const PORT = process.env.PORT || 8080
+const port = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
 module.exports = app
@@ -96,9 +96,9 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () =>
-    console.log(`Mixing it up on port ${PORT}`)
-  )
+  server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 
   // set up our socket control center
   const io = socketio(server)
