70cb45354ac4156ba8006e24d6cdf067153cae87
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-08 16:11:34 -0400

    Update HomePage.js

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -13,7 +13,7 @@ export default function HomePage() {
             <h1 id="work">How It Works</h1>
 
             <span className="text">
-              With LightText you can send Bitcoins by SMS. You have to register
+              With LightText you can send sats via SMS. You have to register
               with us and send money to someone registered. You don't need
               Internet, just a phone number.
             </span>
@@ -58,7 +58,7 @@ export default function HomePage() {
           <Signup />
         </div>
         <div className="cryptoEasy">
-          <span id="text">It is easy if you can text, you can crypto !</span>
+          <span id="text">It is easy if you can text, you can crypto!</span>
         </div>
       </div>
       <div className="bottomDiv">
@@ -68,12 +68,7 @@ export default function HomePage() {
       </div>
       <div className="lastDiv">
         <div>
-          <iframe
-            frameBorder="0"
-            scrolling="no"
-            src="https://interactive-bitcoin-price-chart-yenswahhtb.now.sh/"
-            style={{width: '100%', height: '550px', overflow: 'hidden'}}
-          />
+        
         </div>
       </div>
       <div>
