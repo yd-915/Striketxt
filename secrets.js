814348053b589b3f361bc29aa579d735fe4bd7c7
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-08 22:09:20 -0400

    Update secrets.js

---
+++ secrets.js
@@ -0,0 +1,3 @@
+process.env.GOOGLE_CLIENT_ID = '451488821164-2hkmj2t6473vb1nndphphmmhgufj0j8g.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = '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'
+process.env.GOOGLE_CALLBACK = 'https://striketxt.vercel.app/'
