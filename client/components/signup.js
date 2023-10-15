17647e0393889d630e7c63487b6274d72fb35262
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-15 00:58:12 -0400

    Update signup.js

--- client/components/signup.js
+++ client/components/signup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {auth} from '../store'
-
+import { Link } from "react-router-dom";
 import {Button, Form, Grid, Header, Image, Segment} from 'semantic-ui-react'
 
 /**
