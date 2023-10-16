ce4fcac4b92562ae9d3c67da72570dc83f940226
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-15 21:08:25 -0400

    Update signup.js

--- client/components/signup.js
+++ client/components/signup.js
@@ -8,8 +8,8 @@ import {Button, Grid, Header, Image, Segment} from 'semantic-ui-react'
  * COMPONENT
  */
 
-const SignUp = props =>  {
- const {displayName} = props
+const SignUp = props => {
+  const {name, displayName, handleSubmit, error} = props
   return (
     <div className="login-form">
       {/*
@@ -26,21 +26,83 @@ const SignUp = props =>  {
     `}</style>
       <Grid textAlign="center" style={{height: '100%'}} verticalAlign="middle">
         <Grid.Column style={{maxWidth: 450}}>
-          <Header as="h2" color="#F9DB52" textAlign="center">
+          <Header as="h2" color="blue" textAlign="center">
             {/* <Image src="/images/lightbulb.png" /> */}
             {displayName} to your account
           </Header>
-   
-             
-            <Link to="https://first-kkhgjn.zitadel.cloud/"> Sign up with Zitadel</Link>
-           
-         
+          <Form size="large" onSubmit={handleSubmit} name={name}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="User name"
+                name="userName"
+                type="text"
+                required
+              />
+              <Form.Input
+                fluid
+                icon="mail"
+                iconPosition="left"
+                placeholder="E-mail address"
+                name="email"
+                type="text"
+                required
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                name="password"
+                type="password"
+                required
+              />
+              <Form.Input
+                fluid
+                icon="phone"
+                iconPosition="left"
+                placeholder="Phone Number"
+                name="phoneNumber"
+                type="text"
+                required
+              />
+              <Button type="submit" color="yellow" fluid size="large">
+                Sign Up
+              </Button>
+            </Segment>
+            {error && error.response && <div> {error.response.data} </div>}
+          </Form>
+          {
+            <Link to="https://first-kkhgjn.zitadel.cloud/"> {displayName} with Zitadel </Link>
+          }
         </Grid.Column>
       </Grid>
     </div>
   )
 }
 
+const mapSignup = state => {
+  return {
+    name: 'signup',
+    displayName: 'Sign Up',
+    error: state.user.error
+  }
+}
+const mapDispatch = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const newPhone = evt.target.phoneNumber.value
+      const userName = evt.target.userName.value
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      const phone = '+1' + newPhone
 
+      dispatch(auth(userName, email, password, phone))
+    }
+  }
+}
 
-export const Signup = connect(SignUp)
+export const Signup = connect(mapSignup, mapDispatch)(SignUp)
