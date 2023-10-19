1b2d936f9d18ea618d862251afe605dd795ff646
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2023-10-19 15:40:12 -0400

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import {Navbar, Footer} from './components'
 import Routes from './routes'
 
 const App = () => {
+  useEffect(() => {
+    fetch("https://strikesmsserver-production.up.railway.app/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
     <div>
       <Navbar />
